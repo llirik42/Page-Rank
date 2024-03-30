6d6ffe5623aaed1531e4684aa0861f42456620be
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-03-30 23:25:40 +0700

    POETRY БЛИНБ

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import random
+from habr_parser import parse_articles
 
-from ranking import calculate_ranks, Pair
 
-random.seed(1)
-edges = []
+def main():
+    parse_articles()
 
-for i in range(10):
-    i1 = random.randint(1, 10)
-    i2 = random.randint(1, 10)
 
-    if i1 != i2:
-        edges.append(Pair(i1, i2))
-
-result = calculate_ranks(pairs=edges, precision=5, damping_factor=1)
-print(result)
+if __name__ == '__main__':
+    main()
--- parsing.py
+++
@@ -1,9 +0,0 @@
-from habr_parser import parse_articles
-
-
-def main():
-    parse_articles()
-
-
-if __name__ == '__main__':
-    main()
